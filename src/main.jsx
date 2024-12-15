43c60a0d66930669e1fb159a948e819181dc2141
KashaanAsif <kashaanasif162@gmail.com>
2024-12-15 15:56:48 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById('root')).render(
+<div>
+  <h1>Assignment # 07</h1>
+  <h3>BY Kashaan Asif</h3>
+  <img src="/public/vite.svg" alt="" srcset="" />
+  <img src="/src/assets/react.svg" alt="" srcset="" />
+</div>
+)
